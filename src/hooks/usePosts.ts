2302fe68525b1943b37e67f9be8713d0bde5b419
Muhammad Ali Akbar <maakbarofficial@gmail.com>
2024-07-23 15:54:06 +0500

    Infinite Query

--- src/hooks/usePosts.ts
+++ src/hooks/usePosts.ts
@@ -1,4 +1,4 @@
-import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { keepPreviousData, useInfiniteQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 interface Post {
@@ -9,22 +9,24 @@ interface Post {
 }
 
 interface PostQuery {
-    page: number;
     pageSize: number;
 }
 
-const usePosts = (query: PostQuery) => useQuery<Post[], Error>({
+const usePosts = (query: PostQuery) => useInfiniteQuery<Post[], Error>({
     queryKey: ['posts', query],
-    queryFn: () => axios
+    queryFn: ({ pageParam = 1 }) => axios
         .get('https://jsonplaceholder.typicode.com/posts', {
             params: {
-                _start: (query.page - 1) * query.pageSize,
+                _start: (pageParam - 1) * query.pageSize,
                 _limit: query.pageSize
             }
         })
         .then((res) => (res.data)),
     staleTime: 1 * 60 * 1000, // 1 min
     placeholderData: keepPreviousData,
+    getNextPageParam: (lastPage, allPages) => {
+        return lastPage.length > 0 ? allPages.length + 1 : undefined;
+    }
 })
 
 export default usePosts
