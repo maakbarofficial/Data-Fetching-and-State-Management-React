15d1ac1664329d06b9f8f9e6bdc005c31b58143e
Muhammad Ali Akbar <maakbarofficial@gmail.com>
2024-07-30 19:12:13 +0500

    useReducer Hook

--- src/state-management/TaskList.tsx
+++ src/state-management/TaskList.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useReducer, useState } from "react";
+import taskReducer from "./reducers/tasksReducers";
 
 interface Task {
   id: number;
@@ -6,34 +7,19 @@ interface Task {
 }
 
 const TaskList = () => {
-  const [tasks, setTasks] = useState<Task[]>([]);
+  // const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, dispatch] = useReducer(taskReducer, []);
 
   return (
     <>
-      <button
-        onClick={() =>
-          setTasks([
-            { id: Date.now(), title: 'Task ' + Date.now() },
-            ...tasks,
-          ])
-        }
-        className="btn btn-primary my-3"
-      >
+      <button onClick={() => dispatch({ type: "ADD", task: { id: Date.now(), title: "Task" + Date.now() } })} className="btn btn-primary my-3">
         Add Task
       </button>
       <ul className="list-group">
         {tasks.map((task) => (
-          <li
-            key={task.id}
-            className="list-group-item d-flex justify-content-between align-items-center"
-          >
+          <li key={task.id} className="list-group-item d-flex justify-content-between align-items-center">
             <span className="flex-grow-1">{task.title}</span>
-            <button
-              className="btn btn-outline-danger"
-              onClick={() =>
-                setTasks(tasks.filter((t) => t.id !== task.id))
-              }
-            >
+            <button className="btn btn-outline-danger" onClick={() => dispatch({ type: "DELETE", taskId: task.id })}>
               Delete
             </button>
           </li>
